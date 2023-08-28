722dfa1f962581cd9dfcb21e2436f31c8dd70e0d
James Fennell <jamespfennell@gmail.com>
2023-08-28 18:51:08 -0400

    Add merge sort based algorithm

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,10 +5,19 @@ pub fn generator_1(c: &mut Criterion) {
     c.bench_function("generator1", |b| {
         b.iter(|| {
             let mut generator: Generator1 = Default::default();
-            generator.nth(10000);
+            generator.nth(100000);
         })
     });
 }
 
-criterion_group!(benches, generator_1);
+pub fn generator_3(c: &mut Criterion) {
+    c.bench_function("generator3", |b| {
+        b.iter(|| {
+            let mut generator: Generator3 = Default::default();
+            generator.nth(100000);
+        })
+    });
+}
+
+criterion_group!(benches, generator_1, generator_3);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -47,13 +47,80 @@ impl Iterator for Generator1 {
     }
 }
 
+/// Prime number generator using the algorithm described in [1], but without using a hash map.
+///
+/// ```
+/// # use primes::*;
+/// let mut generator: Generator3 = Default::default();
+/// assert_eq![generator.next(), Some(2)];
+/// assert_eq![generator.next(), Some(3)];
+/// assert_eq![generator.next(), Some(5)];
+/// ```
+///
+/// [1] https://eli.thegreenplace.net/2023/my-favorite-prime-number-generator/
+pub struct Generator3 {
+    d: Vec<(u64, u64)>,
+    buf: Vec<(u64, u64)>,
+    q: u64,
+}
+
+impl Default for Generator3 {
+    fn default() -> Self {
+        Self {
+            d: Default::default(),
+            buf: Default::default(),
+            q: 2,
+        }
+    }
+}
+
+impl Iterator for Generator3 {
+    type Item = u64;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            for (c, p) in &self.d {
+                if *c != self.q {
+                    break;
+                }
+                self.buf.push((c + p, *p));
+            }
+            self.q += 1;
+            if self.buf.is_empty() {
+                let p = self.q - 1;
+                self.d.push((p * p, p));
+                return Some(p);
+            }
+            let mut dest = 0;
+            let mut src_d = self.buf.len();
+            let mut src_buf = 0;
+            while src_buf < self.buf.len() {
+                if src_d == self.d.len() || self.buf[src_buf] < self.d[src_d] {
+                    self.d[dest] = self.buf[src_buf];
+                    dest += 1;
+                    src_buf += 1;
+                } else {
+                    self.d[dest] = self.d[src_d];
+                    dest += 1;
+                    src_d += 1;
+                }
+            }
+            self.buf.clear();
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn the_thousandth_prime() {
+    fn the_thousandth_prime_1() {
         let mut generator: Generator1 = Default::default();
+        assert_eq![generator.nth(1000 - 1), Some(7919)];
+    }
+    #[test]
+    fn the_thousandth_prime_3() {
+        let mut generator: Generator3 = Default::default();
         assert_eq![generator.nth(1000 - 1), Some(7919)];
     }
 }
