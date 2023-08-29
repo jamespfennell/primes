b153f37aea51105f64d15a362801fa22e033efe1
James Fennell <jamespfennell@gmail.com>
2023-08-29 09:48:04 -0400

    Add some anaylsis to estimate the time complexity

--- src/bin/primes.rs
+++ src/bin/primes.rs
@@ -1,9 +1,15 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 fn main() {
-    let range = 0..200;
-    let data: Vec<(usize, u128)> = range
+    let range = 1..=100;
+    let mut ns: Vec<usize> = range.into_iter().map(|i| vec![i; 3]).flatten().collect();
+    let mut rng = thread_rng();
+    ns.shuffle(&mut rng);
+    let data: Vec<(usize, u128)> = ns
         .into_iter()
         .map(|i| {
-            let step = 5000;
+            let step = 10000;
             let n = i * step;
             let start = std::time::Instant::now();
             let mut generator: primes::Generator3 = Default::default();
